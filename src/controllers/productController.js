4cb7a607fa35b1a4d8e4107ab970558d2d4c15b4
ceci21062 <ceci21062@gmail.com>
2019-10-03 14:42:38 -0500

    put cambia el precio de los productos workin progres para productos no existentes

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const JSON = require('circular-json');
-
 const Product = require('../models/product');
+
 
 router.get('/products', (req, res) => {
     const products = Product.find((err, products) => {
@@ -11,5 +11,33 @@ router.get('/products', (req, res) => {
       });
       
 });
+
+router.post('/products', (req, res) => {
+ const { name, price } = req.body;
+ if(typeof name != 'string' || typeof price != 'number') return res.status(400).json({success: false, error: 'Bad Request'});
+ Product.create({ name, price },(err, product) => {
+   if (err) return console.log(err);
+   res.send('Saved');
+ });
+});
+
+
+router.put(`/products/:productid`, async(req, res)=>{
+  Product.updateOne({name: req.body.name}, {$set:{price: req.body.price}},function(err,product) {
+    
+    if(err){
+      console.log("Something wrong when updating data!");
+    }
+    console.log(product);
+    
+    return res.send("cambiado")
+  ;
+  });
+});
+  
+      
+
+
+    
 
 module.exports = router;
