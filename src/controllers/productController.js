3bc89b14bad40e424e09e6a29c667f5398654617
Silvana <silvana15_91@hotmail.com>
2019-10-03 14:23:32 -0500

    Se finalizaron rutas de products

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -5,7 +5,7 @@ const JSON = require('circular-json');
 const Product = require('../models/product');
 
 router.get('/products', (req, res) => {
-    const products = Product.find((err, products) => {
+    Product.find((err, products) => {
         if (err) return console.error(err); 
         res.send(JSON.stringify(products));
         console.log(products);
@@ -20,6 +20,38 @@ router.post('/products', (req, res) => {
     if (err) return console.log(err);
     res.send('Saved');
   }); 
+});
+
+router.put('/products/:productId',(req, res) => {
+    // Validate Request
+    if(!req.body.price) {
+        return res.status(400).send({
+            message: "product content can not be empty"
+        });
+    }
+
+    // Find note and update it with the request body
+    Product.findByIdAndUpdate(req.params.productId, {
+        name: req.body.name || "Untitled Product",
+        price: req.body.price
+    }, {new: true})
+    .then(product => {
+        if(!product) {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.productId
+            });
+        }
+        res.send(product);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Product not found with id " + req.params.productId
+            });                
+        }
+        return res.status(500).send({
+            message: "Error updating product with id " + req.params.productId
+        });
+    });
 });
 
 router.delete('/products/:productId', (req, res) => {
--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Product = require('./models/product');
+
 
 mongoose.connect('mongodb+srv://churro-latte:1qa2ws3ed@cluster0-ardbn.mongodb.net/churro_latte?retryWrites=true&w=majority', {
     useNewUrlParser: true,
